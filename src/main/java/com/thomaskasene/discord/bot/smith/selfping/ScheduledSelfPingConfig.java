913c178d9db50f34e31df45ff9e20e057a552bcb
Thomas Kåsene <6691406+ThomasKasene@users.noreply.github.com>
2021-08-01 00:26:35 +0200

    Instantiating a RestTemplate

--- src/main/java/com/thomaskasene/discord/bot/smith/selfping/ScheduledSelfPingConfig.java
+++ src/main/java/com/thomaskasene/discord/bot/smith/selfping/ScheduledSelfPingConfig.java
@@ -1,19 +1,23 @@
 package com.thomaskasene.discord.bot.smith.selfping;
 
 import com.thomaskasene.discord.bot.smith.ApplicationProperties;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Configuration
-@RequiredArgsConstructor
 public class ScheduledSelfPingConfig {
 
     private final ApplicationProperties applicationProperties;
     private final RestTemplate restTemplate;
+
+    public ScheduledSelfPingConfig(ApplicationProperties applicationProperties, RestTemplateBuilder restTemplateBuilder) {
+        this.applicationProperties = applicationProperties;
+        this.restTemplate = restTemplateBuilder.build();
+    }
 
     @Scheduled(fixedDelay = 10_000L)
     public void executeScheduledSelfPing() {
