331a8c9507d8727e5932a5a0159370b34abb9eaf
Thomas Kåsene <6691406+ThomasKasene@users.noreply.github.com>
2021-08-01 14:00:10 +0200

    Filtered messages that are written by the bot itself

--- src/main/java/com/thomaskasene/discord/bot/smith/DiscordGatewayManager.java
+++ src/main/java/com/thomaskasene/discord/bot/smith/DiscordGatewayManager.java
@@ -4,6 +4,7 @@ import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.annotation.Configuration;
@@ -18,23 +19,28 @@ import static java.util.regex.Pattern.CASE_INSENSITIVE;
 public class DiscordGatewayManager implements DisposableBean {
 
     private final GatewayDiscordClient gatewayDiscordClient;
+    private final User selfUser;
 
     public DiscordGatewayManager(ApplicationProperties applicationProperties) {
         DiscordClient discordClient = DiscordClient.create(applicationProperties.getDiscordBotToken());
         gatewayDiscordClient = discordClient.login().block();
+        selfUser = gatewayDiscordClient.getSelf().block();
 
         Pattern pattern = Pattern.compile("^(smith, )?(.+)(, smith[\\\\?]?)?$", CASE_INSENSITIVE);
 
         gatewayDiscordClient.on(MessageCreateEvent.class).subscribe(messageCreateEvent -> {
             Message message = messageCreateEvent.getMessage();
 
+            if (message.getAuthor().map(user -> user == selfUser).orElse(false)) {
+                return; // This message was posted by us
+            }
+
             log.info("Received a message: {}", message.getContent());
 
             Matcher matcher = pattern.matcher(message.getContent());
 
             if (!matcher.find()) {
-                // This message isn't for us
-                return;
+                return; // This message isn't for us
             }
 
             String command = matcher.group(2);
