00bd847764c8b0e5c13f5a98b83aac18956f1e41
Thomas Kåsene <6691406+ThomasKasene@users.noreply.github.com>
2021-08-01 13:40:24 +0200

    Added a couple of log statements for debugging

--- src/main/java/com/thomaskasene/discord/bot/smith/DiscordGatewayManager.java
+++ src/main/java/com/thomaskasene/discord/bot/smith/DiscordGatewayManager.java
@@ -4,6 +4,7 @@ import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +12,7 @@ import java.util.regex.Pattern;
 
 import static java.util.regex.Pattern.CASE_INSENSITIVE;
 
+@Slf4j
 @Configuration
 public class DiscordGatewayManager implements DisposableBean {
 
@@ -25,10 +27,16 @@ public class DiscordGatewayManager implements DisposableBean {
         gatewayDiscordClient.on(MessageCreateEvent.class).subscribe(messageCreateEvent -> {
             Message message = messageCreateEvent.getMessage();
 
+            log.info("Received a message: {}", message.getContent());
+
             String command = pattern.matcher(message.getContent()).group(2);
 
+            log.info("Resolved the command {}", command);
+
             if (command != null) {
-                message.getChannel().subscribe(channel -> channel.createMessage("You said \"" + command + "\""));
+                message.getChannel().subscribe(channel -> {
+                    channel.createMessage("You said \"" + command + "\"");
+                });
             }
         });
     }
