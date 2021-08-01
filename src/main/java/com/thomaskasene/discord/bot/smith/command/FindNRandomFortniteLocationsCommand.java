66586dd9b9a06a7c9d367588fea00ad201c10295
Thomas Kåsene <6691406+ThomasKasene@users.noreply.github.com>
2021-08-01 20:52:36 +0200

    Converting the first letter of each word in the Fortnite location name to upper-case

--- src/main/java/com/thomaskasene/discord/bot/smith/command/FindNRandomFortniteLocationsCommand.java
+++ src/main/java/com/thomaskasene/discord/bot/smith/command/FindNRandomFortniteLocationsCommand.java
@@ -5,11 +5,16 @@ import discord4j.core.object.entity.Message;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
+import static java.lang.Character.toUpperCase;
 import static java.util.regex.Pattern.CASE_INSENSITIVE;
+import static java.util.stream.Collectors.joining;
 
 @Component
 @RequiredArgsConstructor
@@ -19,8 +24,6 @@ public class FindNRandomFortniteLocationsCommand implements Command {
             "^(?:pick|choose|find(?: us)?|give(?: us)?) (a|some|[0-9]+)(?: random)?(?: fortnite)? location[s]?$",
             CASE_INSENSITIVE);
 
-    private final Random randomGenerator = new Random();
-
     private final FortniteApiService fortniteApiService;
 
     @Override
@@ -28,34 +31,48 @@ public class FindNRandomFortniteLocationsCommand implements Command {
 
         Matcher matcher = applicabilityPattern.matcher(command);
 
-        if (!matcher.find()) {
-            return false;
+        if (matcher.find()) {
+            List<String> namedLocations = fortniteApiService.findNamedLocationNames();
+
+            int desiredLocationCount = translateToLocationCount(matcher.group(1));
+            int limitedLocationCount = Math.min(desiredLocationCount, namedLocations.size());
+
+            List<String> selectedLocations = selectRandomNLocations(namedLocations, limitedLocationCount);
+
+            String resultMessage = createResultMessage(selectedLocations);
+
+            message.getChannel().subscribe(messageChannel -> messageChannel.createMessage(resultMessage).subscribe());
+
+            return true;
         }
 
-        List<String> namedLocations = new LinkedList<>(fortniteApiService.findNamedLocationNames());
+        return false;
+    }
 
-        String amountOfLocations = matcher.group(1);
-
-        int desiredLocationCount;
-
-        if (amountOfLocations.equalsIgnoreCase("a") || amountOfLocations.equals("1")) {
-            desiredLocationCount = 1;
-        } else if (amountOfLocations.equalsIgnoreCase("some")) {
-            desiredLocationCount = 3;
+    private int translateToLocationCount(String command) {
+        if (command.equalsIgnoreCase("a") || command.equals("1")) {
+            return 1;
+        } else if (command.equalsIgnoreCase("some")) {
+            return 3;
         } else {
-            desiredLocationCount = Math.min(Integer.parseInt(amountOfLocations), namedLocations.size());
+            return Integer.parseInt(command);
         }
+    }
 
-        Set<String> chosenLocations = new HashSet<>(desiredLocationCount);
+    private List<String> selectRandomNLocations(List<String> availableLocations, int count) {
+        List<String> shuffledLocations = new ArrayList<>(availableLocations);
+        Collections.shuffle(shuffledLocations);
+        return shuffledLocations.subList(0, count);
+    }
 
-        while (chosenLocations.size() < desiredLocationCount) {
-            chosenLocations.add(namedLocations.remove(randomGenerator.nextInt(namedLocations.size())));
-        }
-
-        String newLineDelimitedLocations = String.join("\n", chosenLocations).toLowerCase();
-
-        message.getChannel().subscribe(messageChannel -> messageChannel.createMessage(newLineDelimitedLocations).subscribe());
-
-        return true;
+    private String createResultMessage(List<String> selectedLocations) {
+        return selectedLocations.stream()
+                .map(locationName -> {
+                    String[] nameParts = locationName.split(" ");
+                    return Stream.of(nameParts)
+                            .map(namePart -> toUpperCase(namePart.charAt(0)) + namePart.substring(1).toLowerCase())
+                            .collect(joining(" "));
+                })
+                .collect(joining("\n"));
     }
 }
